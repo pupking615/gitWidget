76d949e867f52a6ba3a0daf2d79a043a0798b9db
x931216 <150511754+x931216@users.noreply.github.com>
2024-10-07 14:29:02 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QLabel>
 #include <QPushButton>
 #include <QColorDialog>
+#include <QFileDialog>
 #include <QColor>
 #include <QFontDialog>
 #include <QFont>
@@ -24,7 +25,7 @@ int main(int argc, char *argv[])
     layout1->setSpacing(1);
     QLabel *label1 = new QLabel("Leader    50915122 廖御証");
     QLabel *label2 = new QLabel("Member 1 41243214 何維禧");
-    QLabel *label3 = new QLabel("Member 2 41243252 蔡承叡");
+    QLabel *label3 = new QLabel("Member 2 41243252蔡承叡");
     QLabel *label4 = new QLabel("Member 3");
 
     layout1->addWidget(label1);
@@ -55,13 +56,12 @@ int main(int argc, char *argv[])
         label4->setStyleSheet(style);
     });
 
-    // Simplified Member 2 tab with only a button
+    // Member 2 tab with only a button
     QWidget *tab3 = new QWidget();
     QVBoxLayout *layout3 = new QVBoxLayout();
     QPushButton *member2Button = new QPushButton("Change Leader Text Style");
 
     QObject::connect(member2Button, &QPushButton::clicked, [=]() {
-        // Use QFontDialog to change font style
         bool ok;
         QFont font = QFontDialog::getFont(&ok, window);
         if (ok) {
@@ -75,12 +75,23 @@ int main(int argc, char *argv[])
     layout3->addWidget(member2Button);
     tab3->setLayout(layout3);
 
-    // Member 3 tab
+    // Member 3 tab with file selection functionality
     QWidget *tab4 = new QWidget();
     QVBoxLayout *layout4 = new QVBoxLayout();
-    QLabel *member3Label = new QLabel("This is Member 3's tab");
-    QPushButton *member3Button = new QPushButton("Button 3");
-    layout4->addWidget(member3Label);
+    QPushButton *member3Button = new QPushButton("Select File");
+
+    QObject::connect(member3Button, &QPushButton::clicked, [=]() {
+        QString fileName = QFileDialog::getOpenFileName(window, "Open a file", "", "All Files (*)");
+        if (!fileName.isEmpty()) {
+            // Update the labels on the Leader tab with the selected file path
+            QString style = QString("Selected file path: %1").arg(fileName);
+            label1->setText(style);
+            label2->setText(style);
+            label3->setText(style);
+            label4->setText(style);
+        }
+    });
+
     layout4->addWidget(member3Button);
     tab4->setLayout(layout4);
 
